02e29155daee2c9354be8e38af50c5cd1f9b76c4
Adric Net <adric@adric.net>
2016-06-22 10:50:16 -0400

    vol7.sh: get mft as well (long), fix typos, x64

--- vol7.sh
+++ vol7.sh
@@ -1,10 +1,9 @@
 #Win7 vol:
 # define $FILE
-PROFILE=Win7SP1x86
-## redfine this to us another vol binary or include the plugins path
+PROFILE=Win7SP1x64
+## redefine this to us another vol binary or include the plugins path
 VOL_COMM=vol.py 
 ## create and define OUT_FOLDER to not use cwd
-OUT_FOLDER=.
 OUT=text
 
 for p in apihooks autoruns callbacks cmdline cmdscan clipboard consoles dlllist driverirp drivermodule driverscan handles hivelist hivescan iehistory imageinfo ldrmodules malprocfind modscan modules netscan pslist psscan pstree psxview schtasks shellbags ; do 
@@ -12,10 +11,11 @@ echo -n "starting $p - "
 $VOL_COMM --profile $PROFILE -f $FILE --output=$OUT $p > $OUT_FOLDER/$FILE-vol25c-$p.$OUT; done
 echo "$p done"
 
-echo 'starting complex plugins: svcscan V mutantscan S and timeliner'
+echo 'starting complex plugins: svcscan V, malfind, mutantscan S, mftparser (body), and timeliner'
 $VOL_COMM --profile $PROFILE -f $FILE svcscan -v > $OUT_FOLDER/$FILE-vol25c-svcscanv.txt
 $VOL_COMM --profile $PROFILE -f $FILE malfind -v > $OUT_FOLDER/$FILE-vol25c-malfind.txt
-$VOL_COMM --profile $PROFILE -f $FILE mutantscan -s > $OUT_FOLDER/$FILE-vol25c-mutantsv.txt
+$VOL_COMM --profile $PROFILE -f $FILE mutantscan -s > $OUT_FOLDER/$FILE-vol25c-mutants.txt
+$VOL_COMM --profile $PROFILE -f $FILE mftparser --output=body > $OUT_FOLDER/$FILE-vol25c-mftparser-body.txt
 $VOL_COMM --profile $PROFILE -f $FILE timeliner > $OUT_FOLDER/$FILE-vol25c-tl.txt
 echo "Done!"
 
