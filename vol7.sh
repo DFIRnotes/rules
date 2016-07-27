df79d7c91d08dc2b01597ae59fc47546c91a3cf1
Adric Net <adric@adric.net>
2016-07-27 13:57:19 -0400

    Rewrite of volatility batch script, tweaks

--- vol7.sh
+++ vol7.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-### Volatility semi-automated memory image processing, for WinXP images
+### Volatility semi-automated memory image processing, for Windows images
 ### bsk for dfirnotes.org, Copyleft MIT License : https://github.com/DFIRnotes/rules/blob/master/LICENSE
 ### Requirements: SIFT 3 or volatility 2.5.x ; pictures needs PIL and dot available
 ### Run volatility framework anlaysis plugins against a provided image in an opinionated order
@@ -7,8 +7,9 @@
 ### 	2) long run of complex plugins; 3)make pictures
 ### Ref: FOR508, Art of Memory Forensics, Malware Analyst Cookbook
 
-### Win7 x86 (32bit) version
+### Win7 and up version, set your profile appropriately
 
+## always check the profile if things aren't working right!
 VOL_PROFILE=Win7SP1x86
 #define $VOL_FILEIN for your memory image
 ## redefine this to use another vol binary or include more plugins path
@@ -20,6 +21,7 @@ STARS="***Volatility batch***"
 
 ### TODO  
 ## BUGFIX: Set out location to . if env var not set
+## BUGFIX: We're forcing profile on invocation, but this shouldn't be needed?
 ## FEATURE: document how to branch -2
 ## FEATURE: Pull volatility version rather than static string
 ## FEATURE: if file exists and is greater than sizeof(vol usage error), skip the plugin ?
@@ -37,26 +39,26 @@ echo $STARS using Volatility Foundation Volatility Framework 2.5 + Community plu
 echo "$STARS 0) First, quick tables upfront to look for interesting processes"
 for p in pstree malsysproc; do
 echo -n "$p "
-$VOL_COMM -f $VOL_FILEIN $p --output-file=$OUT_FOLDER/$VOL_FILEIN-vol25c-$p.txt  ; done
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE $p --output-file=$OUT_FOLDER/$VOL_FILEIN-vol25c-$p.txt  ; done
 echo; echo "$STARS 0)Quick tables completed. 1)Starting batch plugin processing ..."
 
 ## do the whole batch of data processing, simple arguments
 for q in apihooks autoruns callbacks cmdline cmdscan clipboard consoles dlllist driverirp drivermodule driverscan getsids iehistory handles hivelist hivescan imageinfo ldrmodules malfind malprocfind modscan modules psscan psxview schtasks shellbags ; do 
 
 echo -n " $q, "
-$VOL_COMM -f $VOL_FILEIN $q  > $OUT_FOLDER/$VOL_FILEIN-vol25c-$q.txt; done
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE $q  > $OUT_FOLDER/$VOL_FILEIN-vol25c-$q.txt; done
 echo "$STARS 1) Batch processing, simple plugin arguments done"
 
 echo "$STARS 2) Starting complex plugins: pstotal DOT, svcscan V, mutantscan N, mftparser BODY, and timeliner"
-$VOL_COMM -f $VOL_FILEIN pstotal --output=dot  > $OUT_FOLDER/$VOL_FILEIN-vol25c-pstotal.dot
-$VOL_COMM -f $VOL_FILEIN svcscan -v  > $OUT_FOLDER/$VOL_FILEIN-vol25c-svcscanv.txt
-$VOL_COMM -f $VOL_FILEIN mutantscan -s  > $OUT_FOLDER/$VOL_FILEIN-vol25c-mutantsv.txt
-$VOL_COMM -f $VOL_FILEIN mftparser --output=body  > $OUT_FOLDER/$VOL_FILEIN-vol25c-mftparser-body.txt 
-$VOL_COMM -f $VOL_FILEIN timeliner  > $OUT_FOLDER/$VOL_FILEIN-vol25c-tl.txt
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE pstotal --output=dot  > $OUT_FOLDER/$VOL_FILEIN-vol25c-pstotal.dot
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE svcscan -v  > $OUT_FOLDER/$VOL_FILEIN-vol25c-svcscanv.txt
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE mutantscan -s  > $OUT_FOLDER/$VOL_FILEIN-vol25c-mutantsv.txt
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE mftparser --output=body  > $OUT_FOLDER/$VOL_FILEIN-vol25c-mftparser-body.txt 
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE timeliner  > $OUT_FOLDER/$VOL_FILEIN-vol25c-tl.txt
 
 echo "$STARS 3) Make pictures!"
 dot -T png $OUT_FOLDER/$VOL_FILEIN-vol25c-pstotal.dot > $OUT_FOLDER/$VOL_FILEIN-vol25c-pstotal.png
-$VOL_COMM -f $VOL_FILEIN screenshot -D $OUT_FOLDER 
+$VOL_COMM -f $VOL_FILEIN --profile $VOL_PROFILE screenshot -D $OUT_FOLDER 
 
 echo "$STARS Volatility batch run on $VOL_FILEIN completed!"
 
