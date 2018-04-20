5eeb6e5d1405b51c9a81b287e2fca80f9c7334fa
Adric Net <adric@adric.net>
2018-04-20 16:55:00 +0000

    Quick script to download and mount hnfc files
    The original "HoneyNet Forensics Challenge" http://old.honeynet.org/challenge/index.html

---
+++ mount_hnfc.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+## Grab the original HoneyNet challenge files from hn.org, extract and mount them.
+## TODO: It would be good to check if the files exists before making with the downloading (again)
+
+mkdir /cases/hnfc; cd /cases/hnfc
+
+wget http://old.honeynet.org/misc/files/challenge-images.tar
+tar xf challenge-images.tar
+for i in *.gz; do gunzip $i; done
+
+sudo mkdir -p /mnt/hnfc
+
+sudo mount -o loop,ro /cases/hnfc/honeypot.hda8.dd /mnt/hnfc/
+sudo mount -o loop,ro /cases/hnfc/honeypot.hda1.dd /mnt/hnfc/boot
+sudo mount -o loop,ro /cases/hnfc/honeypot.hda6.dd /mnt/hnfc/home
+sudo mount -o loop,ro /cases/hnfc/honeypot.hda5.dd /mnt/hnfc/usr
+sudo mount -o loop,ro /cases/hnfc/honeypot.hda7.dd /mnt/hnfc/var
+
+echo 'To chroot: sudo chroot /mnt/hnfc /bin/bash'
